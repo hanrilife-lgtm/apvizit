fd5053a987346623a29ae57bdb34ed74e8dffa7b
hanrilife-lgtm <hanrilife@gmail.com>
2026-07-10 08:45:42 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    TELEGRAM_TOKEN: '8553303488:AAEH5C-2CYs-4ry5sC9iXVR1b6ZOm4TW4Ic',
+    TELEGRAM_CHAT_ID: '6658068919',
+    VK_PROFILE_URL: 'https://vk.com/idsanapolozkov'
+};
+
+window.CONFIG = CONFIG;
